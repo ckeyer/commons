4a6660ea7ffa11bd77622290d58b7b77b475adf6
ckeyer <me@ckeyer.com>
2015-10-31 00:29:11 +0800

    修改mysql

--- db/mysql.go
+++ db/mysql.go
@@ -9,49 +9,41 @@ import (
 
 type MySqlConfig struct {
 	Host     string
-	Port     string
+	Port     int
 	User     string
 	Password string
 	Database string
-
-	ConnStr string
 }
-
-var (
-	ms *MySqlConfig
-)
 
 func Conn(connStr string) (db *sql.DB, err error) {
 
-	db, err := sql.Open("mysql", connStr)
+	db, err = sql.Open("mysql", connStr)
 	if err != nil {
 		return
 	}
 	if err = db.Ping(); err != nil {
 		return
 	}
+	return
 }
 
 // GetConnStr 获取连接字符串
-func GetConnStr() string {
-	if ms != nil {
-		if ms.ConnStr != "" {
-			return ms.ConnStr
+func (m *MySqlConfig) GetConnStr() string {
+	if m != nil {
+		if m.Host == "" {
+			m.Host = "localhost"
 		}
-		if ms.Host == "" {
-			ms.Host = "localhost"
+		if m.Port == 0 {
+			m.Port = 3306
 		}
-		if ms.Port == "" {
-			ms.Port = "3306"
+		if m.User == "" {
+			m.User = "root"
 		}
-		if ms.User == "" {
-			ms.User = "root"
+		if m.Password == "" {
+			m.Password = "root"
 		}
-		if ms.Password == "" {
-			ms.Password = "root"
-		}
-		ms.ConnStr = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", m.User, m.Password, m.Host, m.Port, m.Database)
-		return ms.ConnStr
+		connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", m.User, m.Password, m.Host, m.Port, m.Database)
+		return connStr
 	}
 	return ""
 }
