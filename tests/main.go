12b8a6922b0bea7ffd3a6cd5a76f44ef47735aef
Chuanjian Wang <me@ckeyer.com>
2016-07-12 19:23:30 +0800

    add struct test

--- tests/main.go
+++ tests/main.go
@@ -13,4 +13,20 @@ func init() {
 func main() {
 	log.Debug("start ")
 	log.Info(viper.GetString("url"))
+	log.Info(viper.GetString("author.name"))
+
+	var u User
+	err := viper.UnmarshalKey("author", &u)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	log.Info(u)
+
+}
+
+type User struct {
+	Name  string `yaml:"name"`
+	Email string `yaml:"email`
 }
