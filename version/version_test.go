ddbc281fdd07257042e7000ecad132f9adb6165c
Chuanjian Wang <me@ckeyer.net>
2018-01-01 23:30:23 +0800

    use gopkg.in/check.v1

--- version/version_test.go
+++ version/version_test.go
@@ -4,16 +4,24 @@ import (
 	"testing"
 
 	"github.com/Masterminds/semver"
+	check "gopkg.in/check.v1"
 )
 
-func TestV(t *testing.T) {
-	v, err := semver.NewVersion("v1.2.3-beta.1+build345")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	v2, _ := v.SetMetadata("asdfwer")
+func Test(t *testing.T) { check.TestingT(t) }
 
-	t.Logf("%#v", v)
-	t.Logf("%#v", v2)
+type S struct{}
+
+var _ = check.Suite(&S{})
+
+func (s *S) TestV(c *check.C) {
+	v, err := semver.NewVersion("v1.2.3-beta.1+build345")
+	c.Assert(err, check.IsNil)
+	c.Assert(v.Major(), check.Equals, int64(1))
+	c.Assert(v.Minor(), check.Equals, int64(2))
+	c.Assert(v.Patch(), check.Equals, int64(3))
+	c.Assert(v.Metadata(), check.Equals, "build345")
+
+	v2, err := v.SetMetadata("asdfwer")
+	c.Assert(err, check.IsNil)
+	c.Assert(v2.Metadata(), check.Equals, "asdfwer")
 }
