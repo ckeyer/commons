eddb81cfd32f103538a312ad95afe14750ddcd6f
Chuanjian Wang <me@ckeyer.net>
2018-01-03 09:07:27 +0800

    fix http

--- file/tar.go
+++ file/tar.go
@@ -1,167 +1,65 @@
 package file
 
-import (
-	"archive/tar"
-	"compress/gzip"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
-)
+// func Tar() {
+// 	buf := &bytes.Buffer{}
+// 	tw := tar.NewWriter(buf)
+// 	defer tw.Close()
 
-type tarContext struct {
-	src string
-	dst string
-	gz  bool
-}
+// 	for _, dir := range []string{".", "conf.d"} {
+// 		hdr := &tar.Header{
+// 			Name:       dir,
+// 			Mode:       0755,
+// 			Uid:        os.Getuid(),
+// 			Gid:        os.Getgid(),
+// 			ModTime:    time.Now(),
+// 			Typeflag:   tar.TypeDir,
+// 			AccessTime: time.Now(),
+// 			ChangeTime: time.Now(),
+// 		}
+// 		err := tw.WriteHeader(hdr)
+// 		if err != nil {
+// 			log.Debugln("WriteHeader,", err)
+// 			return nil, err
+// 		}
+// 	}
+// 	for _, file := range filelist {
+// 		hdr := &tar.Header{
+// 			Name:       file.Path,
+// 			Mode:       int64(file.Perm),
+// 			Uid:        os.Getuid(),
+// 			Gid:        os.Getgid(),
+// 			Size:       int64(len(file.Content)),
+// 			ModTime:    time.Now(),
+// 			Typeflag:   tar.TypeReg,
+// 			AccessTime: time.Now(),
+// 			ChangeTime: time.Now(),
+// 		}
+// 		err := tw.WriteHeader(hdr)
+// 		if err != nil {
+// 			log.Debugln("WriteHeader,", err)
+// 			return nil, err
+// 		}
+// 		n, err := tw.Write(file.Content)
+// 		if err != nil {
+// 			log.Debugln("Write,", err, n)
+// 			return nil, err
+// 		}
+// 		tw.Flush()
+// 	}
 
-func Tar(src, dst string) error {
-	return newTarContext(src, dst, false).tar()
-}
+// 	bs := buf.Bytes()
 
-func Tgz(src, dst string) error {
-	return newTarContext(src, dst, true).tar()
-}
+// 	fullpath := "nginx.tar"
+// 	filename, _, typ := parseFilename(fullpath)
 
-func UnTar(targetFile, to string) error {
-	return newTarContext(to, targetFile, false).untar()
-}
-
-func UnTgz(targetFile, to string) error {
-	return newTarContext(to, targetFile, true).untar()
-}
-
-func newTarContext(src, dst string, gz bool) *tarContext {
-	return &tarContext{
-		src: src,
-		dst: dst,
-		gz:  gz,
-	}
-}
-
-func (tc *tarContext) tar() error {
-	f, err := tc.targetFile()
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	var fw io.WriteCloser
-	if tc.gz {
-		fw = gzip.NewWriter(f)
-		defer fw.Close()
-	} else {
-		fw = f
-	}
-
-	tw := tar.NewWriter(fw)
-	defer tw.Close()
-
-	return tc.writeTar(tw)
-}
-
-func (tc *tarContext) targetFile() (*os.File, error) {
-	_, err := os.Stat(tc.dst)
-	if os.IsNotExist(err) {
-		_, err := os.Stat(filepath.Dir(tc.dst))
-		if os.IsNotExist(err) {
-			err = os.MkdirAll(filepath.Dir(tc.dst), 0755)
-			if err != nil {
-				return nil, err
-			}
-		}
-	} else {
-		err = os.Remove(tc.dst)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return os.OpenFile(tc.dst, os.O_CREATE|os.O_RDWR, 0644)
-}
-
-func (tc *tarContext) writeTar(tw *tar.Writer) error {
-	return filepath.Walk(tc.src, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() && path == tc.src {
-			return nil
-		}
-
-		hd, err := tar.FileInfoHeader(info, "")
-		if err != nil {
-			return err
-		}
-		hd.Name = trimDirPrefix(path, tc.src)
-		tw.WriteHeader(hd)
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-		_, err = io.Copy(tw, file)
-		if err != nil {
-			return err
-
-		}
-		return nil
-	})
-}
-
-func (tc *tarContext) untar() error {
-	f, err := os.Open(tc.dst)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	var tr *tar.Reader
-	if tc.gz {
-		gr, err := gzip.NewReader(f)
-		if err != nil {
-			return err
-		}
-		defer gr.Close()
-
-		tr = tar.NewReader(gr)
-	} else {
-		tr = tar.NewReader(f)
-	}
-
-	for {
-		hd, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-
-		localName := filepath.Join(tc.src, hd.Name)
-		if hd.FileInfo().IsDir() {
-			os.MkdirAll(localName, 0755)
-			continue
-		}
-
-		fw, err := os.OpenFile(localName, os.O_CREATE|os.O_RDWR, 0644)
-		if err != nil {
-			return err
-		}
-
-		_, err = io.CopyN(fw, tr, hd.Size)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func trimDirPrefix(path, dir string) string {
-	return strings.TrimPrefix(path, strings.TrimSuffix(dir, "/")+"/")
-}
+// 	sha1, size := SHA1(bs)
+// 	return &nevis_common.File{
+// 		Name:    filename,
+// 		Path:    filepath.Join(rootdir, filename),
+// 		Type:    typ,
+// 		Perm:    0644,
+// 		Size_:   size,
+// 		Sha1:    sha1,
+// 		Content: bs,
+// 	}, nil
+// }
--- file/tar_test.go
+++ file/tar_test.go
@@ -6,27 +6,27 @@ import (
 )
 
 func TestTar(t *testing.T) {
-	err := Tar("./", "/tmp/tartest.tar")
+	err := TarFile("./", "/tmp/tartest.tar")
 	if err != nil {
 		t.Error(err)
 	}
 
 	os.MkdirAll("/tmp/tar/out", 0755)
 
-	err = UnTar("/tmp/tartest.tar", "/tmp/tar/out")
+	err = UnTarFile("/tmp/tartest.tar", "/tmp/tar/out")
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestTgz(t *testing.T) {
-	err := Tgz("./", "/tmp/tgztest.tar.gz")
+	err := TgzFile("./", "/tmp/tgztest.tar.gz")
 	if err != nil {
 		t.Error(err)
 	}
 
 	os.MkdirAll("/tmp/tgz/out", 0755)
-	err = UnTgz("/tmp/tgztest.tar.gz", "/tmp/tgz/out")
+	err = UnTgzFile("/tmp/tgztest.tar.gz", "/tmp/tgz/out")
 	if err != nil {
 		t.Error(err)
 	}
